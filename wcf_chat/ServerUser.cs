d204e3d2b37c7564d9e9c6986ff5f8b9d4091420
NRR <pianzovmaxsim236@gmail.com>
2024-07-22 18:36:59 +0300

    fix

--- wcf_chat/ServerUser.cs
+++ wcf_chat/ServerUser.cs
@@ -2,7 +2,7 @@
 namespace wcf_chat
 {
     public class ServerUser
-    {
+    { 
         public int ID { get; set; }
         public string Name { get; set; }
         public OperationContext operationContext { get; set; }
