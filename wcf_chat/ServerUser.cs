51d825488b147d36be89574d4dec3f648b5405f2
NR <95234863+sraww@users.noreply.github.com>
2025-05-16 16:16:10 +0300

    Update ServerUser.cs

--- wcf_chat/ServerUser.cs
+++ wcf_chat/ServerUser.cs
@@ -2,8 +2,7 @@ using System.ServiceModel;
 namespace wcf_chat
 {
     public class ServerUser
-        
-    {
+        {
         public int ID { get; set; }
         public string Name { get; set; }
         public OperationContext operationContext { get; set; }
