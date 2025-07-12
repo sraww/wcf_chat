227c2435f9c2a4c46a022877c986747b2b7cb796
NR <95234863+sraww@users.noreply.github.com>
2025-07-12 11:18:08 +0300

    Update AssemblyInfo.cs

--- ChatHost/Properties/AssemblyInfo.cs
+++ ChatHost/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
