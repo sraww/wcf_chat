aa1439cb97b8060d15788f22e6ced8ef795b08e4
NR <95234863+sraww@users.noreply.github.com>
2025-07-13 19:01:06 +0300

    Update ServiceChat.cs

--- wcf_chat/ServiceChat.cs
+++ wcf_chat/ServiceChat.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
+
 namespace wcf_chat
 {
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
